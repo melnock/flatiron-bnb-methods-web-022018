727e7d3212c36a1c6cfc4a48a185bcede1b59151
github username <github email address>
2018-03-22 13:11:24 -0400

    first chunk done

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,45 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(span_start, span_end)
+    span_start_date = Date.parse(span_start)
+    span_end_date = Date.parse(span_end)
+    span = (span_start_date..span_end_date).to_a
+    self.listings.select do |listing|
+      listing_arr = []
+      listing.reservations.map do |res|
+        listing_arr << [res.checkin, res.checkout]
+      end
+      res_dates = listing_arr.flatten
+      !span.any?{|el| res_dates.include?(el)}
+    end
+  end
 
+  def self.highest_ratio_res_to_listings
+    cities = City.all
+    hash = {}
+    cities.map do |city|
+      listing_total = city.listings.count
+      res_total = 0
+      city.listings.each do |listing|
+        res_total = listing.reservations.count + res_total
+      end
+      hash[city] = (res_total.to_f / listing_total)
+    end
+    hash.max_by{|k,v| v}[0]
+  end
+
+  def self.most_res
+    cities = City.all
+    hash = {}
+    cities.map do |city|
+      res_total = 0
+      city.listings.each do |listing|
+        res_total = listing.reservations.count + res_total
+      end
+      hash[city] = res_total
+    end
+    hash.max_by{|k,v| v}[0]
+  end
+
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,27 @@
 class Listing < ActiveRecord::Base
+  after_create :check_user_host
+  after_destroy :check_user_host
+
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood
+
+  def average_review_rating
+    
+  end
+
+  private
+
+
+  def check_user_host
+    if self.host.listings.empty?
+      self.host.host = false
+    else
+      self.host.host = true
+    end
+    self.host.save
+  end
 end
